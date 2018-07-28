fd0761c81c29fd2a5c46928e529bf52721ca655c
Simon Ganon <ganonsimon@gmail.com>
2018-07-28 22:54:15 +0200

    maintain: split function arg_parser

--- src/arg_parser/arg_parser.c
+++ src/arg_parser/arg_parser.c
@@ -6,27 +6,38 @@
 /*   By: sganon <sganon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/28 17:20:15 by sganon            #+#    #+#             */
-/*   Updated: 2018/07/28 22:33:13 by sganon           ###   ########.fr       */
+/*   Updated: 2018/07/28 22:40:42 by sganon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ssl.h"
 
-void arg_parser(t_ssl *ssl, int argc, char **argv)
+static int setSSLFunction(t_ssl *ssl, char *f)
 {
   int ssl_func;
+
+  ssl_func = toSSLFunction(f);
+  if (ssl_func < 0)
+    return -1;
+  ssl->current_function = ssl_func;
+  return ssl->current_function;
+}
+
+void arg_parser(t_ssl *ssl, int argc, char **argv)
+{
+  int err;
+
   if (argc == 1)
   {
     print_help(0);
-    return ;
+    return;
   }
-  
-  ssl_func = toSSLFunction(argv[1]);
-  if (ssl_func < 0)
+
+  err = setSSLFunction(ssl, argv[1]);
+  if (err < 0)
   {
     print_available_funcs(argv[1]);
-    return ;
+    return;
   }
-  ssl->current_function = ssl_func;
   // ft_putstr(getSSLFunction(ssl->current_function));
 }
